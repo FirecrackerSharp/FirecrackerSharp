283411e015a9fc80b6135ffe07444b2c6da6d41b
kanpov <karpovanton729@gmail.com>
2024-05-14 13:24:15 +0300

    Document the FirecrackerSharp.Installation namespace

--- FirecrackerSharp/Installation/FirecrackerInstall.cs
+++ FirecrackerSharp/Installation/FirecrackerInstall.cs
@@ -1,5 +1,15 @@
 namespace FirecrackerSharp.Installation;
 
+/// <summary>
+/// A reference to an installation of Firecracker containing the minimum binaries required for the SDK to operate:
+/// the firecracker and the jailer.
+///
+/// For one install, retrieve this via <see cref="FirecrackerInstaller"/>, and for multiple installs use <see cref="FirecrackerInstallManager"/>
+/// instead. However, if both approaches aren't sufficient, creating this object manually is also viable.
+/// </summary>
+/// <param name="Version">The version (GitHub tag) of this Firecracker installation</param>
+/// <param name="FirecrackerBinary">The absolute path to the firecracker binary, which must be executable</param>
+/// <param name="JailerBinary">The absolute path to the jailer binary, which must be executable</param>
 public record FirecrackerInstall(
     string Version,
     string FirecrackerBinary,
--- FirecrackerSharp/Installation/FirecrackerInstallManager.cs
+++ FirecrackerSharp/Installation/FirecrackerInstallManager.cs
@@ -3,6 +3,20 @@ using FirecrackerSharp.Host;
 
 namespace FirecrackerSharp.Installation;
 
+/// <summary>
+/// An automated tool to manage multiple simultaneous installations of Firecracker. This is the recommended approach
+/// for facilitating any Firecracker environment, as it ensures compability with multiple installations and can retrieve
+/// the latest versions of Firecracker (which is likely more up-to-date than the one provided by your distribution,
+/// unless you use AWS).
+///
+/// To persist metadata about multiple installations, a JSON index file is used internally with no restrictions on
+/// how many concurrent installations it can encapsulate.
+/// </summary>
+/// <param name="storagePath">The absolute path to the directory, which contains both the index file and all
+/// installations that were created via this <see cref="FirecrackerInstallManager"/></param>
+/// <param name="jsonSerializerOptions">The <see cref="JsonSerializerOptions"/> that are used to save and load the
+/// index file</param>
+/// <param name="indexFilename">The filename of the index</param>
 public class FirecrackerInstallManager(
     string storagePath,
     JsonSerializerOptions jsonSerializerOptions,
@@ -14,9 +28,27 @@ public class FirecrackerInstallManager(
     };
     private string IndexPath => IHostFilesystem.Current.JoinPaths(storagePath, indexFilename);
     
+    /// <summary>
+    /// An overload of the primary constructor for <see cref="FirecrackerInstallManager"/> that allows you not to
+    /// specify the <see cref="JsonSerializerOptions"/>, instead using the default ones that have all default settings
+    /// other than snake case naming for data.
+    /// </summary>
+    /// <param name="storagePath">The absolute path to the storage directory</param>
+    /// <param name="indexFilename">The index filename, "index.json" by default</param>
     public FirecrackerInstallManager(string storagePath, string indexFilename = "index.json") 
         : this(storagePath, DefaultSerializerOptions, indexFilename) {}
 
+    /// <summary>
+    /// Perform an installation of Firecracker to this <see cref="FirecrackerInstallManager"/>'s specified storage by
+    /// using <see cref="FirecrackerInstaller"/> internally.
+    ///
+    /// This method does NOT save the acquired <see cref="FirecrackerInstall"/> to the index! To do this, call
+    /// <see cref="AddToIndexAsync"/> with the returned <see cref="FirecrackerInstall"/> value.
+    /// </summary>
+    /// <param name="releaseTag"></param>
+    /// <param name="repoOwner"></param>
+    /// <param name="repoName"></param>
+    /// <returns></returns>
     public async Task<FirecrackerInstall> InstallAsync(string releaseTag = "latest",
         string repoOwner = "firecracker-microvm", string repoName = "firecracker")
     {
@@ -24,6 +56,10 @@ public class FirecrackerInstallManager(
         return await installer.InstallAsync();
     }
     
+    /// <summary>
+    /// Add a given <see cref="FirecrackerInstall"/> to the index file (duplicates are allowed).
+    /// </summary>
+    /// <param name="addedInstall">The <see cref="FirecrackerInstall"/> to be added.</param>
     public async Task AddToIndexAsync(FirecrackerInstall addedInstall)
     {
         if (!File.Exists(IndexPath))
@@ -39,6 +75,10 @@ public class FirecrackerInstallManager(
         await WriteIndexAsync(installs);
     }
 
+    /// <summary>
+    /// Add several <see cref="FirecrackerInstall"/>'s to the index file.
+    /// </summary>
+    /// <param name="addedInstalls">An <see cref="IEnumerable{T}"/> with all <see cref="FirecrackerInstall"/>s to be added</param>
     public async Task AddManyToIndexAsync(IEnumerable<FirecrackerInstall> addedInstalls)
     {
         if (!File.Exists(IndexPath))
@@ -52,29 +92,44 @@ public class FirecrackerInstallManager(
         await WriteIndexAsync(installs);
     }
 
+    /// <summary>
+    /// Retrieve all <see cref="FirecrackerInstall"/>s currently stored in the index file.
+    /// </summary>
+    /// <returns>A <see cref="List{T}"/> with all <see cref="FirecrackerInstall"/>s from the index</returns>
     public async Task<List<FirecrackerInstall>> GetAllFromIndexAsync()
     {
         var indexJson = await IHostFilesystem.Current.ReadTextFileAsync(IndexPath);
         return JsonSerializer.Deserialize<List<FirecrackerInstall>>(indexJson, jsonSerializerOptions)!;
     }
 
+    /// <summary>
+    /// Retrieve a <see cref="FirecrackerInstall"/> with the given version from the index file, if such a <see cref="FirecrackerInstall"/>
+    /// is present.
+    /// </summary>
+    /// <param name="version">The version to match for</param>
+    /// <param name="strict">Whether to match for exactly this version, or to use this version as a substring</param>
+    /// <returns>The fetched <see cref="FirecrackerInstall"/> or null if none was found</returns>
     public async Task<FirecrackerInstall?> GetFromIndexAsync(string version, bool strict = false)
     {
         var installs = await GetAllFromIndexAsync();
-        return FindInIndex(installs, version, strict);
+        return FindInIndex(installs, version, strict).FirstOrDefault();
     }
 
-    public async Task<bool> RemoveFromIndexAsync(string version, bool strict = false)
+    /// <summary>
+    /// Remove all occurrences of a <see cref="FirecrackerInstall"/> with the given version from the index file.
+    /// </summary>
+    /// <param name="version">The version to match for</param>
+    /// <param name="strict">Whether to match for exactly this version, or to use this version as a substring</param>
+    public async Task RemoveFromIndexAsync(string version, bool strict = false)
     {
         var installs = await GetAllFromIndexAsync();
-        var install = FindInIndex(installs, version, strict);
-        if (install is null) return false;
-        
-        installs.Remove(install);
+        installs.RemoveAll(x => strict ? x.Version == version : x.Version.Contains(version));
         await WriteIndexAsync(installs);
-        return true;
     }
 
+    /// <summary>
+    /// Remove all <see cref="FirecrackerInstall"/>s stored in the index file.
+    /// </summary>
     public async Task RemoveAllFromIndexAsync()
     {
         await WriteIndexAsync([]);
@@ -86,10 +141,10 @@ public class FirecrackerInstallManager(
         await IHostFilesystem.Current.WriteTextFileAsync(IndexPath, json);
     }
 
-    private static FirecrackerInstall? FindInIndex(IEnumerable<FirecrackerInstall> installs, string version, bool strict = false)
+    private static IEnumerable<FirecrackerInstall> FindInIndex(IEnumerable<FirecrackerInstall> installs, string version, bool strict = false)
     {
         return strict
-            ? installs.FirstOrDefault(x => x.Version == version)
-            : installs.FirstOrDefault(x => x.Version.Contains(version));
+            ? installs.Where(x => x.Version == version)
+            : installs.Where(x => x.Version.Contains(version));
     }
 }
