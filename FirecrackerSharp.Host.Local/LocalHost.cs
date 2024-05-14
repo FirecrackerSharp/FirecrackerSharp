057604fbd65f94a479f045061e3f586b80ce8323
kanpov <karpovanton729@gmail.com>
2024-05-14 14:03:40 +0300

    Finish documenting the remaining minimum of current APIs

--- FirecrackerSharp.Host.Local/LocalHost.cs
+++ FirecrackerSharp.Host.Local/LocalHost.cs
@@ -2,13 +2,23 @@ using Serilog;
 
 namespace FirecrackerSharp.Host.Local;
 
+/// <summary>
+/// An SDK Linux host that is a local Linux machine.
+///
+/// This host offers by far the best performance as it eliminates the overhead of either containerization (Podman) or
+/// communication to a remote host through the network (SSH). This is why we highly recommend using the local host for
+/// any production needs unless remote communication is strictly necessary!
+/// </summary>
 public static class LocalHost
 {
+    /// <summary>
+    /// Configure the local host to be used internally by the SDK.
+    /// </summary>
     public static void Configure()
     {
         IHostFilesystem.Current = new LocalHostFilesystem();
         IHostProcessManager.Current = new LocalHostProcessManager();
-        IHostSocketManager.Current = new LocalHostSocketManager();
+        IHostSocketManager.Current = new LocalHostSocketManager();  
         
         Log.Information("Using local (Linux) host for FirecrackerSharp");
     }
