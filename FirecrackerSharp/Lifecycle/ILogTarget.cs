1cb782b38a0d19658613a23cdaa04e79038ba70d
kanpov <karpovanton729@gmail.com>
2024-06-19 17:42:37 +0300

    Start work on adding XML docs to 0.5-beta APIs

--- FirecrackerSharp/Lifecycle/ILogTarget.cs
+++ FirecrackerSharp/Lifecycle/ILogTarget.cs
@@ -3,18 +3,46 @@ using FirecrackerSharp.Host;
 
 namespace FirecrackerSharp.Lifecycle;
 
+/// <summary>
+/// A log target is anything that can receive logs emitted by the firecracker/jailer process, which are sent line-by-line
+/// in a reactive (observer) manner. An example would be an in-memory <see cref="StringBuilder"/>, a file and so forth.
+/// </summary>
 public interface ILogTarget
 {
+    /// <summary>
+    /// Receive the streamed log line and persist it to this target. This operation is synchronous, meaning that only
+    /// synchronous I/O operations can be used when needed (except for firing up an asynchronous task on a background
+    /// thread).
+    /// </summary>
+    /// <param name="line">The streamed line</param>
     public void Receive(string line);
 
     internal static readonly ILogTarget Null = new NullLogTarget();
     
+    /// <summary>
+    /// Create a new log target that logs to the given in-memory <see cref="StringBuilder"/>.
+    /// </summary>
+    /// <param name="stringBuilder">The <see cref="StringBuilder"/> that should receive the logs</param>
+    /// <returns>The created log target</returns>
     public static ILogTarget ToStringBuilder(StringBuilder stringBuilder)
         => new StringBuilderLogTarget(stringBuilder);
 
+    /// <summary>
+    /// Create a new log target that logs to the given stream.
+    /// </summary>
+    /// <param name="stream">The <see cref="Stream"/> to be written to</param>
+    /// <returns>The created log target</returns>
     public static ILogTarget ToStream(Stream stream)
         => new StreamLogTarget(stream);
 
+    /// <summary>
+    /// Create a new log target that logs to the given file, plus create that file on the app or VM host if it doesn't
+    /// already exist.
+    /// </summary>
+    /// <param name="filePath">The absolute path to the file</param>
+    /// <param name="onAppHost">Whether the file should be on the application host or the VM host. If these are the
+    /// same machine, this doesn't have any influence</param>
+    /// <returns>The created log target</returns>
     public static ILogTarget ToFile(string filePath, bool onAppHost = true)
     {
         switch (onAppHost)
@@ -29,6 +57,12 @@ public interface ILogTarget
         return new FileLogTarget(filePath, onAppHost);
     }
 
+    /// <summary>
+    /// Create a new log target that logs to a combination of given log targets. This is internally implemented by simply
+    /// looping through the given targets and sequentially invoking their 
+    /// </summary>
+    /// <param name="aggregatedTargets">The <see cref="IEnumerable{T}"/> of aggregated log targets</param>
+    /// <returns>The created log target</returns>
     public static ILogTarget ToAggregate(IEnumerable<ILogTarget> aggregatedTargets)
         => new AggregateLogTarget(aggregatedTargets);
 
--- FirecrackerSharp/Lifecycle/NotAccessibleDueToLifecycleException.cs
+++ FirecrackerSharp/Lifecycle/NotAccessibleDueToLifecycleException.cs
@@ -1,3 +1,8 @@
 namespace FirecrackerSharp.Lifecycle;
 
+/// <summary>
+/// An exception that is raised when a microVM's resource is attempted to be accessed that is not accessible under
+/// the current <see cref="VmLifecyclePhase"/> of this microVM.
+/// </summary>
+/// <param name="message">The message detailing what exactly the issue was</param>
 public sealed class NotAccessibleDueToLifecycleException(string message) : Exception(message);
